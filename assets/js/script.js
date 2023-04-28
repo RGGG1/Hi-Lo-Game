63c3662d1cd3938c0d65cfabd9bc5d388648dfd4
RGGG1 <>
2023-04-28 16:26:19 +0000

    got the random images generating constantly, added nine individual button areas, changed the score / lives text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,117 @@
+var myelemet = document.getElementById('myimg1'),
+    myimgs =["/assets/images/banana-yellow.png","/assets/images/monkey-black-white.png","/assets/images/banana-black-white.png","/assets/images/monkey-yellow.png"];
+function changeImg(myelemet,myimgs){
+  'use stict';
+  setInterval(function(){
+    var myRandomNum = Math.floor(Math.random() * myimgs.length);
+
+    myelemet.src = myimgs[myRandomNum];
+  }, 250);
+}
+changeImg(myelemet,myimgs);
+
+var myelemet = document.getElementById('myimg2'),
+    myimgs =["/assets/images/banana-yellow.png","/assets/images/monkey-black-white.png","/assets/images/banana-black-white.png","/assets/images/monkey-yellow.png"];
+function changeImg(myelemet,myimgs){
+  'use stict';
+  setInterval(function(){
+    var myRandomNum = Math.floor(Math.random() * myimgs.length);
+
+    myelemet.src = myimgs[myRandomNum];
+  }, 250);
+}
+changeImg(myelemet,myimgs);
+
+var myelemet = document.getElementById('myimg3'),
+    myimgs =["/assets/images/banana-yellow.png","/assets/images/monkey-black-white.png","/assets/images/banana-black-white.png","/assets/images/monkey-yellow.png"];
+function changeImg(myelemet,myimgs){
+  'use stict';
+  setInterval(function(){
+    var myRandomNum = Math.floor(Math.random() * myimgs.length);
+
+    myelemet.src = myimgs[myRandomNum];
+  }, 250);
+}
+changeImg(myelemet,myimgs);
+
+
+var myelemet = document.getElementById('myimg4'),
+    myimgs =["/assets/images/banana-yellow.png","/assets/images/monkey-black-white.png","/assets/images/banana-black-white.png","/assets/images/monkey-yellow.png"];
+function changeImg(myelemet,myimgs){
+  'use stict';
+  setInterval(function(){
+    var myRandomNum = Math.floor(Math.random() * myimgs.length);
+
+    myelemet.src = myimgs[myRandomNum];
+  }, 250);
+}
+changeImg(myelemet,myimgs);
+
+var myelemet = document.getElementById('myimg5'),
+    myimgs =["/assets/images/banana-yellow.png","/assets/images/monkey-black-white.png","/assets/images/banana-black-white.png","/assets/images/monkey-yellow.png"];
+function changeImg(myelemet,myimgs){
+  'use stict';
+  setInterval(function(){
+    var myRandomNum = Math.floor(Math.random() * myimgs.length);
+
+    myelemet.src = myimgs[myRandomNum];
+  }, 250);
+}
+changeImg(myelemet,myimgs);
+
+var myelemet = document.getElementById('myimg6'),
+    myimgs =["/assets/images/banana-yellow.png","/assets/images/monkey-black-white.png","/assets/images/banana-black-white.png","/assets/images/monkey-yellow.png"];
+function changeImg(myelemet,myimgs){
+  'use stict';
+  setInterval(function(){
+    var myRandomNum = Math.floor(Math.random() * myimgs.length);
+
+    myelemet.src = myimgs[myRandomNum];
+  }, 250);
+}
+changeImg(myelemet,myimgs);
+
+
+var myelemet = document.getElementById('myimg7'),
+    myimgs =["/assets/images/banana-yellow.png","/assets/images/monkey-black-white.png","/assets/images/banana-black-white.png","/assets/images/monkey-yellow.png"];
+function changeImg(myelemet,myimgs){
+  'use stict';
+  setInterval(function(){
+    var myRandomNum = Math.floor(Math.random() * myimgs.length);
+
+    myelemet.src = myimgs[myRandomNum];
+  }, 250);
+}
+changeImg(myelemet,myimgs);
+
+
+var myelemet = document.getElementById('myimg8'),
+    myimgs =["/assets/images/banana-yellow.png","/assets/images/monkey-black-white.png","/assets/images/banana-black-white.png","/assets/images/monkey-yellow.png"];
+function changeImg(myelemet,myimgs){
+  'use stict';
+  setInterval(function(){
+    var myRandomNum = Math.floor(Math.random() * myimgs.length);
+
+    myelemet.src = myimgs[myRandomNum];
+  }, 250);
+}
+changeImg(myelemet,myimgs);
+
+var myelemet = document.getElementById('myimg9'),
+    myimgs =["/assets/images/banana-yellow.png","/assets/images/monkey-black-white.png","/assets/images/banana-black-white.png","/assets/images/monkey-yellow.png"];
+function changeImg(myelemet,myimgs){
+  'use stict';
+  setInterval(function(){
+    var myRandomNum = Math.floor(Math.random() * myimgs.length);
+
+    myelemet.src = myimgs[myRandomNum];
+  }, 250);
+}
+changeImg(myelemet,myimgs);
+
+
+
+//
 var images = [];
 
 images[0] = ['/assets/images/banana-yellow.png'];
