530a01c3e2f8914e1b1b3d5a9d5274fb1b0fd1a1
RGGG1 <>
2023-04-29 16:29:06 +0000

    typos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,108 +1,108 @@
-var myelemet = document.getElementById('myimg1'),
+var myelement = document.getElementById('myimg1'),
     myimgs =["/assets/images/banana-yellow.png","/assets/images/monkey-black-white.png","/assets/images/banana-black-white.png","/assets/images/monkey-yellow.png"];
-function changeImg(myelemet,myimgs){
+function changeImg(myelement,myimgs){
   setInterval(function(){
     var myRandomNum = Math.floor(Math.random() * myimgs.length);
 
-    myelemet.src = myimgs[myRandomNum];
+    myelement.src = myimgs[myRandomNum];
   }, 250);
 }
-changeImg(myelemet,myimgs);
+changeImg(myelement,myimgs);
 
 
 
 
 
-var myelemet = document.getElementById('myimg2'),
+var myelement = document.getElementById('myimg2'),
     myimgs =["/assets/images/banana-yellow.png","/assets/images/monkey-black-white.png","/assets/images/banana-black-white.png","/assets/images/monkey-yellow.png"];
-function changeImg(myelemet,myimgs){
+function changeImg(myelement,myimgs){
   setInterval(function(){
     var myRandomNum = Math.floor(Math.random() * myimgs.length);
 
-    myelemet.src = myimgs[myRandomNum];
+    myelement.src = myimgs[myRandomNum];
   }, 250);
 }
-changeImg(myelemet,myimgs);
+changeImg(myelement,myimgs);
 
-var myelemet = document.getElementById('myimg3'),
+var myelement = document.getElementById('myimg3'),
     myimgs =["/assets/images/banana-yellow.png","/assets/images/monkey-black-white.png","/assets/images/banana-black-white.png","/assets/images/monkey-yellow.png"];
-function changeImg(myelemet,myimgs){
+function changeImg(myelement,myimgs){
   setInterval(function(){
     var myRandomNum = Math.floor(Math.random() * myimgs.length);
 
-    myelemet.src = myimgs[myRandomNum];
+    myelement.src = myimgs[myRandomNum];
   }, 250);
 }
-changeImg(myelemet,myimgs);
+changeImg(myelement,myimgs);
 
 
-var myelemet = document.getElementById('myimg4'),
+var myelement = document.getElementById('myimg4'),
     myimgs =["/assets/images/banana-yellow.png","/assets/images/monkey-black-white.png","/assets/images/banana-black-white.png","/assets/images/monkey-yellow.png"];
-function changeImg(myelemet,myimgs){
+function changeImg(myelement,myimgs){
   setInterval(function(){
     var myRandomNum = Math.floor(Math.random() * myimgs.length);
 
-    myelemet.src = myimgs[myRandomNum];
+    myelement.src = myimgs[myRandomNum];
   }, 250);
 }
-changeImg(myelemet,myimgs);
+changeImg(myelement,myimgs);
 
-var myelemet = document.getElementById('myimg5'),
+var myelement = document.getElementById('myimg5'),
     myimgs =["/assets/images/banana-yellow.png","/assets/images/monkey-black-white.png","/assets/images/banana-black-white.png","/assets/images/monkey-yellow.png"];
-function changeImg(myelemet,myimgs){
+function changeImg(myelement,myimgs){
   setInterval(function(){
     var myRandomNum = Math.floor(Math.random() * myimgs.length);
 
-    myelemet.src = myimgs[myRandomNum];
+    myelement.src = myimgs[myRandomNum];
   }, 250);
 }
-changeImg(myelemet,myimgs);
+changeImg(myelement,myimgs);
 
-var myelemet = document.getElementById('myimg6'),
+var myelement = document.getElementById('myimg6'),
     myimgs =["/assets/images/banana-yellow.png","/assets/images/monkey-black-white.png","/assets/images/banana-black-white.png","/assets/images/monkey-yellow.png"];
-function changeImg(myelemet,myimgs){
+function changeImg(myelement,myimgs){
   setInterval(function(){
     var myRandomNum = Math.floor(Math.random() * myimgs.length);
 
-    myelemet.src = myimgs[myRandomNum];
+    myelement.src = myimgs[myRandomNum];
   }, 250);
 }
-changeImg(myelemet,myimgs);
+changeImg(myelement,myimgs);
 
 
-var myelemet = document.getElementById('myimg7'),
+var myelement = document.getElementById('myimg7'),
     myimgs =["/assets/images/banana-yellow.png","/assets/images/monkey-black-white.png","/assets/images/banana-black-white.png","/assets/images/monkey-yellow.png"];
-function changeImg(myelemet,myimgs){
+function changeImg(myelement,myimgs){
   setInterval(function(){
     var myRandomNum = Math.floor(Math.random() * myimgs.length);
 
-    myelemet.src = myimgs[myRandomNum];
+    myelement.src = myimgs[myRandomNum];
   }, 250);
 }
-changeImg(myelemet,myimgs);
+changeImg(myelement,myimgs);
 
 
-var myelemet = document.getElementById('myimg8'),
+var myelement = document.getElementById('myimg8'),
     myimgs =["/assets/images/banana-yellow.png","/assets/images/monkey-black-white.png","/assets/images/banana-black-white.png","/assets/images/monkey-yellow.png"];
-function changeImg(myelemet,myimgs){
+function changeImg(myelement,myimgs){
   setInterval(function(){
     var myRandomNum = Math.floor(Math.random() * myimgs.length);
 
-    myelemet.src = myimgs[myRandomNum];
+    myelement.src = myimgs[myRandomNum];
   }, 250);
 }
-changeImg(myelemet,myimgs);
+changeImg(myelement,myimgs);
 
-var myelemet = document.getElementById('myimg9'),
+var myelement = document.getElementById('myimg9'),
     myimgs =["/assets/images/banana-yellow.png","/assets/images/monkey-black-white.png","/assets/images/banana-black-white.png","/assets/images/monkey-yellow.png"];
-function changeImg(myelemet,myimgs){
+function changeImg(myelement,myimgs){
   setInterval(function(){
     var myRandomNum = Math.floor(Math.random() * myimgs.length);
 
-    myelemet.src = myimgs[myRandomNum];
+    myelement.src = myimgs[myRandomNum];
   }, 250);
 }
-changeImg(myelemet,myimgs);
+changeImg(myelement,myimgs);
 
 
 
